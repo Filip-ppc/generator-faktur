6e8a650248d7ea46d22578283158f9bb8bf5d3b1
Filip-ppc <steinerf.ppc@gmail.com>
2024-12-05 13:54:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Vyhledání elementů
 const form = document.getElementById('fakturaForm');
 const fakturaDiv = document.getElementById('faktura');
+const pdfButton = document.getElementById('pdfButton');
 
 // Funkce pro aktualizaci faktury
 function updateFaktura() {
@@ -9,12 +10,16 @@ function updateFaktura() {
     const cena = form.cena.value;
     const mnozstvi = form.mnozstvi.value;
 
+    // Výpočet celkové ceny
+    const celkovaCena = cena * mnozstvi;
+
     // Vytvoření HTML kódu faktury
     const fakturaHTML = `
         <div class="faktura-polozka">
             <p>Název: ${nazev}</p>
-            <p>Cena: ${cena} Kč</p>
+            <p>Cena za kus: ${cena} Kč</p>
             <p>Množství: ${mnozstvi}</p>
+            <p>Celková cena: ${celkovaCena} Kč</p>
         </div>
     `;
 
@@ -22,8 +27,23 @@ function updateFaktura() {
     fakturaDiv.innerHTML = fakturaHTML;
 }
 
-// Připojení event listeneru na odeslání formuláře
+// Funkce pro vytvoření PDF
+function createPDF() {
+    const doc = new jsPDF();
+
+    const fakturaElement = document.getElementById('faktura');
+
+    doc.html(fakturaElement, {
+        callback: function(doc) {
+            doc.save('faktura.pdf');
+        }
+    });
+}
+
+// Připojení event listenerů
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     updateFaktura();
 });
+
+pdfButton.addEventListener('click', createPDF);
